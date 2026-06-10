5811fb56380aabf7c9a70f79784f4e7156effce9
soaringmz <102166753+soaringmz@users.noreply.github.com>
2026-06-10 22:43:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
+
 from loguru import logger
 from PyQt5.QtWidgets import QApplication
 from src.gui import SteamLoginGUI
 
 if __name__ == "__main__":
     logger.info("Starting application...")
-    sys.stdout.write("\x1b]2;Steam EYA\x07")
+
+    if sys.stdout is not None:
+        sys.stdout.write("\x1b]2;Steam EYA\x07")
 
     app = QApplication(sys.argv)
     ex = SteamLoginGUI("1.0")
